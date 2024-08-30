3b440c39819cc77c1a3f26916d8050158c150160
Oveshen Govender <oveshen.govender@gmail.com>
2024-08-30 21:12:46 +1000

    Updated the header-data.ts

--- server/api/header-data.ts
+++ server/api/header-data.ts
@@ -1,15 +1,19 @@
 // server/api/header-data.js
-import { useStrapi3 } from '#strapi';
-
 export default defineEventHandler(async (event) => {
-  const strapi = useStrapi3();
-  try {
-    const response = await strapi.find('headers');
-    return Array.isArray(response.data) && response.data.length > 0
-      ? response.data[0].attributes
-      : null;
-  } catch (error) {
-    console.error('Error fetching header data:', error);
-    return null;
-  }
-});
+    const config = useRuntimeConfig();
+    const strapiUrl = config.public.strapiUrl;
+  
+    try {
+      const response = await fetch(`${strapiUrl}/api/headers`);
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+      const data = await response.json();
+      return Array.isArray(data.data) && data.data.length > 0
+        ? data.data[0].attributes
+        : null;
+    } catch (error) {
+      console.error('Error fetching header data:', error);
+      return null;
+    }
+  });
