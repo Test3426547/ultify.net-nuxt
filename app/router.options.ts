3725d156c1bbf6817ac1fca5f8511808b1051a48
Oveshen Govender <oveshen.govender@gmail.com>
2024-08-30 19:34:24 +1000

    Initial Commit

--- app/router.options.ts
+++ app/router.options.ts
@@ -1,23 +1,8 @@
 import type { RouterConfig } from '@nuxt/schema'
 
-// https://router.vuejs.org/api/#routeroptions
 export default <RouterConfig>{
   scrollBehavior(to, from, savedPosition) {
-    // If the route has a hash, scroll to it
-    if (to.hash) {
-      return {
-        el: to.hash,
-        top: 0,
-        behavior: 'instant'
-      }
-    }
-
-    // If it's a page reload, maintain the current scroll position
-    if (from.path === to.path) {
-      return savedPosition || { left: 0, top: 0 }
-    }
-
-    // For navigation to a new page, scroll to top instantly
-    return { left: 0, top: 0, behavior: 'instant' }
+    // Always return to the top of the page, no scrolling
+    return { left: 0, top: 0 }
   }
 }
