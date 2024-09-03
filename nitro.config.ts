c1ede8ab00d4ed996fa71befd7ec30fb5d4319a3
Oveshen Govender <oveshen.govender@gmail.com>
2024-09-03 20:52:29 +1000

    Updated to use Pinia

--- nitro.config.ts
+++ nitro.config.ts
@@ -3,7 +3,7 @@ import { defineNitroConfig } from 'nitropack'
 export default defineNitroConfig({
   storage: {
     kv: {
-      driver: 'vercel-kv',
+      driver: 'redis',
       clientOptions: {
         url: process.env.KV_REST_API_URL,
         token: process.env.KV_REST_API_TOKEN,
