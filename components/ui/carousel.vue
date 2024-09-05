942964b3c964fd3c002110f8a10e95c555a8cf14
Oveshen Govender <oveshen.govender@gmail.com>
2024-09-05 14:50:25 +1000

    Implemented ISR in HeaderOther

--- components/ui/carousel.vue
+++ components/ui/carousel.vue
@@ -1,41 +1,4 @@
 <script setup lang="ts">
-import { ref, provide } from 'vue'
-import { ChevronUp, ChevronDown } from 'lucide-vue-next'
-
-const props = defineProps<{
-  opts?: any
-  orientation?: 'horizontal' | 'vertical'
-  class?: string
-}>()
-
-const api = ref(null)
-
-provide('carousel', api)
-
-const prevSlide = () => {
-  api.value?.scrollPrev()
-}
-
-const nextSlide = () => {
-  api.value?.scrollNext()
-}
-</script>
-
-<template>
-  <div :class="['relative', props.class]">
-    <slot :api="api"></slot>
-    <div v-if="orientation === 'vertical'" class="absolute -left-12 top-1/2 -translate-y-1/2 flex flex-col gap-2">
-      <CarouselPrevious @click="prevSlide" />
-      <CarouselNext @click="nextSlide" />
-    </div>
-    <div v-else class="absolute -bottom-12 left-1/2 -translate-x-1/2 flex gap-2">
-      <CarouselPrevious @click="prevSlide" />
-      <CarouselNext @click="nextSlide" />
-    </div>
-  </div>
-</template>
-
-<script setup lang="ts">
 import { ref, provide, defineComponent, h } from 'vue'
 import { ChevronUp, ChevronDown } from 'lucide-vue-next'
 
@@ -93,6 +56,13 @@ const CarouselNext = defineComponent({
     }, h(ChevronDown, { class: 'w-6 h-6' }))
   },
 })
+
+defineExpose({
+  CarouselContent,
+  CarouselItem,
+  CarouselPrevious,
+  CarouselNext
+})
 </script>
 
 <template>
@@ -110,11 +80,7 @@ const CarouselNext = defineComponent({
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
-
-export default defineComponent({
+export default {
   name: 'Carousel',
-})
-
-export { CarouselContent, CarouselItem, CarouselPrevious, CarouselNext }
+}
 </script>
