920f38e4fe762c3a60fd95c315c2c7e242f3638d
Oveshen Govender <oveshen.govender@gmail.com>
2024-09-14 11:03:52 +1000

    Updated the mobile view

--- components/views/home/HomeHeader.vue
+++ components/views/home/HomeHeader.vue
@@ -1,45 +1,49 @@
 <template>
-  <header class="relative h-screen overflow-hidden" v-if="headerData">
+  <header class="relative min-h-screen overflow-hidden" v-if="headerData">
     <div class="absolute inset-0 bg-white h-1/2"></div>
     <div class="absolute inset-x-0 bottom-0 bg-emerald-500 h-1/2"></div>
     <div class="container mx-auto h-full">
       <div class="flex flex-col lg:flex-row h-full">
-        <div class="lg:w-7/12 flex flex-col py-5 relative">
-          <div class="absolute top-1/2 -mt-[300px] -left-[120px] right-0 z-10">
-            <h1 class="text-4xl lg:text-5xl font-bold text-emerald-500 mb-4">
-              {{ headerData.Title }}
-            </h1>
-            <p class="text-lg text-emerald-500 mt-5">
-              {{ headerData.Subtitle }}
-            </p>
+        <div class="w-full lg:w-7/12 flex flex-col py-5 relative">
+          <div class="lg:absolute lg:top-1/2 lg:-mt-[300px] lg:-left-[120px] lg:right-0 z-10 px-4 lg:px-0 mt-16">
+            <div class="bg-white lg:bg-transparent p-8 lg:p-0 rounded-lg lg:rounded-none">
+              <h1 class="text-3xl md:text-4xl lg:text-5xl font-bold text-emerald-500 mb-4 text-center lg:text-left">
+                {{ headerData.Title }}
+              </h1>
+              <p class="text-lg text-emerald-500 mt-5 text-center lg:text-left">
+                {{ headerData.Subtitle }}
+              </p>
+            </div>
           </div>
-          <div class="absolute top-1/2 mt-[20px] -left-[120px] right-0 z-10">
-            <h2 class="text-4xl lg:text-5xl font-bold text-white mb-4">
-              {{ headerData.Heading }}
-            </h2>
-            <p class="text-lg text-white mb-8">
-              {{ headerData.Subheading }}
-            </p>
-            <div class="grid grid-cols-3 gap-4 max-w-3xl">
-              <NuxtLink 
-                v-for="link in headerData.Link" 
-                :key="link.id" 
-                :to="link.Link" 
-                class="btn btn-outline text-white border-white border-2 hover:bg-white hover:text-emerald-500 transition-all duration-300 text-sm px-4 py-3 rounded-full whitespace-nowrap font-extrabold transform hover:-translate-y-1 flex items-center justify-center"
-              >
-                {{ link.Text }}
-              </NuxtLink>
+          <div class="lg:absolute lg:top-1/2 lg:mt-[20px] lg:-left-[120px] lg:right-0 z-10 px-4 lg:px-0 mt-8">
+            <div class="bg-emerald-500 lg:bg-transparent p-8 lg:p-0 rounded-lg lg:rounded-none">
+              <h2 class="text-3xl md:text-4xl lg:text-5xl font-bold text-white mb-4 text-center lg:text-left">
+                {{ headerData.Heading }}
+              </h2>
+              <p class="text-lg text-white mb-8 text-center lg:text-left">
+                {{ headerData.Subheading }}
+              </p>
+              <div class="grid grid-cols-2 md:grid-cols-3 gap-4 max-w-3xl mx-auto lg:mx-0">
+                <NuxtLink 
+                  v-for="link in headerData.Link" 
+                  :key="link.id" 
+                  :to="link.Link" 
+                  class="btn btn-outline text-white border-white border-2 hover:bg-white hover:text-emerald-500 transition-all duration-300 text-sm px-4 py-3 rounded-full whitespace-nowrap font-extrabold transform hover:-translate-y-1 flex items-center justify-center"
+                >
+                  {{ link.Text }}
+                </NuxtLink>
+              </div>
             </div>
           </div>
         </div>
-        <div class="lg:w-5/12 flex justify-center items-center relative">
+        <div class="lg:w-5/12 hidden lg:flex justify-center items-center relative">
           <div class="w-full max-w-md ml-[80px] -mt-[50px] z-10">
             <ContactForm @submit="handleSubmit" />
           </div>
         </div>
       </div>
     </div>
-    <svg class="absolute bottom-5 left-1/2 transform -translate-x-1/2 animate-bounce" width="40" height="35" viewBox="0 0 40 35" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <svg class="absolute bottom-5 left-1/2 transform -translate-x-1/2 animate-bounce hidden md:block" width="40" height="35" viewBox="0 0 40 35" fill="none" xmlns="http://www.w3.org/2000/svg">
       <path d="M20 35L36.5 18.5L33.25 15.25L23.5 25V0H16.5V25L6.75 15.25L3.5 18.5L20 35Z" fill="white"/>
     </svg>
   </header>
