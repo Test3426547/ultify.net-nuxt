a6d6db33dd732aa15923defd3855b07f68bdac6f
Oveshen Govender <oveshen.govender@gmail.com>
2024-09-14 15:17:15 +1000

    Updated the mobile view

--- components/views/home/HomeHeader.vue
+++ components/views/home/HomeHeader.vue
@@ -6,7 +6,7 @@
       <div class="flex flex-col lg:flex-row h-full lg:mt-[150px]">
         <div class="w-full lg:w-7/12 flex flex-col py-5 relative">
           <div class="lg:absolute lg:top-1/2 lg:-mt-[300px] lg:-left-[120px] lg:right-0 z-10 px-4 lg:px-0 mt-16">
-            <div class="flex flex-col justify-center min-h-[40vh] lg:min-h-0 lg:h-auto">
+            <div class="flex flex-col justify-center min-h-[35vh] lg:min-h-0 lg:h-auto">
               <h1 class="text-3xl md:text-4xl lg:text-5xl font-bold text-emerald-500 mb-4 text-center lg:text-left">
                 {{ headerData.Title }}
               </h1>
@@ -16,7 +16,7 @@
             </div>
           </div>
           <div class="lg:absolute lg:top-1/2 lg:mt-[20px] lg:-left-[120px] lg:right-0 z-10 px-4 lg:px-0 mt-8">
-            <div class="flex flex-col justify-center min-h-[50vh] lg:min-h-0 lg:h-auto pt-8 lg:pt-0">
+            <div class="flex flex-col justify-center min-h-[45vh] lg:min-h-0 lg:h-auto pt-4 lg:pt-0">
               <h2 class="text-3xl md:text-4xl lg:text-5xl font-bold text-white mb-4 text-center lg:text-left">
                 {{ headerData.Heading }}
               </h2>
@@ -28,7 +28,7 @@
                   v-for="link in headerData.Link" 
                   :key="link.id" 
                   :to="link.Link" 
-                  class="btn btn-outline text-white border-white border-2 hover:bg-white hover:text-emerald-500 transition-all duration-300 text-sm px-4 py-3 rounded-full whitespace-normal font-extrabold transform hover:-translate-y-1 flex items-center justify-center h-[70px] md:h-[80px] md:w-[calc(100%+20px)] md:max-w-[220px]"
+                  class="btn btn-outline text-white border-white border-2 hover:bg-white hover:text-emerald-500 transition-all duration-300 text-sm px-4 py-3 rounded-full whitespace-normal font-extrabold transform hover:-translate-y-1 flex items-center justify-center h-[70px] lg:h-auto lg:aspect-[3/1]"
                 >
                   {{ link.Text }}
                 </NuxtLink>
